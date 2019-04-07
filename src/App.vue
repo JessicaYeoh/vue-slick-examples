b7189b846cc5655752d7061eda2669433ba6b7f8
Jessica Yeoh <jessica.yeoh@jadonsol.com>
2019-04-07 20:10:55 +1000

    vue slick examples
    - horizontal slick
    - vertical slick
    - autoplay image slider

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <h2>Vue Slick</h2>
+
+        <h4>Horizontal Quantity selector</h4>
+        <vue-slick class="slick-horizontal" :slides-to-show="5" :variable-width="true">
+            <button class="slick_button_class" v-for="i in 100">
+                {{ i }}
+            </button>
+        </vue-slick>
+
+        <hr/>
+
+        <h4>Vertical Quantity selector</h4>
+        <vue-slick :slides-to-show="1" :vertical="true" :vertical-swiping="true">
+            <button class="slick_button_class" v-for="i in 100">
+                {{ i }}
+            </button>
+        </vue-slick>
+
+        <hr/>
+
+        <h4>Image Carousel Autoplay</h4>
+        <vue-slick :slides-to-show="3" :infinite="true" :autoplay="true">
+            <img src="./assets/bordercollie.jpg"/>
+            <img src="./assets/husky.jpg"/>
+            <img src="./assets/jackrussell.jpg"/>
+            <img src="./assets/labrador.jpg"/>
+            <img src="./assets/miniature-dachshund2.jpg"/>
+            <img src="./assets/spitz.jpeg"/>
+        </vue-slick>
+
+        <hr/>
+    </div>
+</template>
+
+<script>
+    import VueSlick from './components/VueSlickComponent.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            VueSlick,
+        }
+    }
+</script>
+
+<style lang="scss" src="./styles/main.scss"></style>
---
+++ src/components/VueSlickComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <slick ref="slick" :options="slickOptions">
+            <slot></slot>
+        </slick>
+    </div>
+</template>
+
+<script>
+    import Slick from 'vue-slick';
+
+    export default {
+        name: 'VueSlickComponent',
+        components: {
+            Slick,
+        },
+        props: {
+            slidesToShow: Number,
+            vertical: {
+                type: Boolean,
+                default: false,
+            },
+            verticalSwiping: {
+                type: Boolean,
+                default: false,
+            },
+            infinite: {
+                type: Boolean,
+                default: false,
+            },
+            autoplay: {
+                type: Boolean,
+                default: false,
+            },
+            variableWidth: {
+                type: Boolean,
+                default: false,
+            },
+        },
+        data() {
+            return {
+                slickOptions: {
+                    slidesToShow: this.slidesToShow,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    focusOnSelect: true,
+                    swipeToSlide: true,
+                    arrows: false,
+                    infinite: this.infinite,
+                    vertical: this.vertical,
+                    verticalSwiping: this.verticalSwiping,
+                    autoplay: this.autoplay,
+                    autoplaySpeed: 2000,
+                    variableWidth: this.variableWidth,
+                },
+            }
+        },
+    }
+</script>
